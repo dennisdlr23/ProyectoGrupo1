5884e3187e18b47aacd4632c6b39f94296c76d52
CarlosCanales <carloscanales722@gmail.com>
2022-11-21 11:42:49 -0600

    video 3 listo
    Cambiar el player por Hellen, Listo!

--- Assets/Scripts/MoveWithFloor.cs
+++ Assets/Scripts/MoveWithFloor.cs
@@ -11,17 +11,22 @@ public class MoveWithFloor : MonoBehaviour
     Vector3 lastGroundPosition; //Almacenamos la ultima posicion conocida del suelo
     string groundName;          //Almacenamos el nombre actual del suelo
     string lastGroundName;      //Almacenamos el nombre del ultimo suelo conocido
-    GameObject groundIn;
+    //GameObject groundIn;
 
-    LayerMask finalmask;
+    //LayerMask finalmask;
+    Quaternion actualRot; 
+    Quaternion lastRot;
+
+    public Vector3 originOffset;
+    public float factorDivision = 4.2f;
 
     // Start is called before the first frame update
     void Start()
     {
         player = this.GetComponent<CharacterController>(); //Inicializamos la variable player almacenando el componente CharacterController
-        var layer1 = 9;
-        var layer2 = 12;
-        finalmask = ~((1 << layer1) | (1 << layer2));
+        // var layer1 = 9;
+        // var layer2 = 12;
+        // finalmask = ~((1 << layer1) | (1 << layer2));
     }
 
     // Update is called once per frame
@@ -30,20 +35,33 @@ public class MoveWithFloor : MonoBehaviour
         if (player.isGrounded)    //En caso de estar tocando el suelo
         {
             RaycastHit hit; //Creamos una variable para almacenar la colision del RayCast
-            if (Physics.Raycast(transform.position, Vector3.down, out hit, 3, finalmask)) //Y creamos una SphereCast, que es como un RayCast pero grueso, dandole un diametro y una direccion en la que "disparar" ese SphereCast
+            if (Physics.SphereCast(transform.position + originOffset, player.radius / factorDivision, -transform.up, out hit)) //Y creamos una SphereCast, que es como un RayCast pero grueso, dandole un diametro y una direccion en la que "disparar" ese SphereCast
             {
-                groundIn = hit.collider.gameObject; //Comprobmamos cual es el suelo actual y almacenamos su GameObject en una variable temporal
-                groundName = groundIn.name;         //despues comprobamos el nombre del suelo
-                groundPosition = groundIn.transform.position; //Una vez que tenemos el GameObject localizado, almacenamos su posicion
+               GameObject groundedIn = hit.collider.gameObject;
+                groundName = groundedIn.name;         //despues comprobamos el nombre del suelo              
+                groundPosition = groundedIn.transform.position; //Una vez que tenemos el GameObject localizado, almacenamos su posicion
+                  //groundIn = hit.collider.gameObject; //Comprobmamos cual es el suelo actual y almacenamos su GameObject en una variable temporal
+                actualRot = groundedIn.transform.rotation;
+
+
                 if ((groundPosition != lastGroundPosition) && (groundName == lastGroundName)) //Si su posicion es distinta de la ultima posicion conocida y el nombre sigue siendo el mismo
                 {
                     this.transform.position += groundPosition - lastGroundPosition; //Sumamos a nuestra posicion la diferencia entre la posicion actual del suelo y la ultimam posicion conocida del suelo.
-                    player.enabled = false;
-                    player.transform.position = this.transform.position;
-                    player.enabled = true;
+                    // player.enabled = false;
+                    // player.transform.position = this.transform.position;
+                    // player.enabled = true;
                 }
                 lastGroundName = groundName;
                 lastGroundPosition = groundPosition;
+
+                if(actualRot != lastRot && groundName == lastGroundName)
+                {
+                    var newRot = this.transform.rotation * (actualRot.eulerAngles - lastRot.eulerAngles);
+                    this.transform.RotateAround(groundedIn.transform.position, Vector3.up, newRot.y);
+                }
+                lastGroundName = groundName;
+                lastGroundPosition = groundPosition;
+                lastRot = actualRot;
 
             }
         }
@@ -51,13 +69,14 @@ public class MoveWithFloor : MonoBehaviour
         {
             lastGroundName = null;
             lastGroundPosition = Vector3.zero;
+            lastRot = Quaternion.Euler(0, 0, 0);
         }
 
     }
-    //Aqui unicamente creamos un gizmo para poder comprobar si el diametro del SphereCast es adecuado al tamaño de nuestro jugador.
+    //Aqui unicamente creamos un gizmo para poder comprobar si el diametro del SphereCast es adecuado al tamaï¿½o de nuestro jugador.
     private void OnDrawGizmos()
     {
         player = this.GetComponent<CharacterController>();
-        Gizmos.DrawRay(transform.position, Vector3.down * 3);
+        Gizmos.DrawWireSphere(transform.position + originOffset, player.radius / factorDivision);
     }
 }
